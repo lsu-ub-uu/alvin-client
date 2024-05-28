b9b81c55dd481fdec889de0c05efdf6bdc66c135
TheExtrapolatingSnowYakShow <kentingemarbergman@gmail.com>
2024-05-28 14:30:17 +0200

    Platslista, navbar och platser
    Platslista och platser har blivit stylade med Tailwind och preppade för införande av HTMX. Navbar reviderad.

--- alvin_django/alvin_place_list/views.py
+++ alvin_django/alvin_place_list/views.py
@@ -10,24 +10,39 @@ def alvin_place_list(request):
     xml_headers_list = {'Content-Type':'application/vnd.uub.recordList+xml','Accept':'application/vnd.uub.recordList+xml'}
 
     # För previewmiljön
-    preview_place_list_url = 'https://cora.epc.ub.uu.se/alvin/rest/record/searchResult/placeSearch?searchData={"name":"placeSearch","children":[{"name":"include","children":[{"name":"includePart","children":[{"name":"placeSearchTerm","value":"**"}]}]}]}'
+    preview_place_list_base_url = 'https://cora.epc.ub.uu.se/alvin/rest/record/searchResult/placeSearch?searchData={"name":"placeSearch","children":[{"name":"include","children":[{"name":"includePart","children":[{"name":"placeSearchTerm","value":"**"}]}]}]}'
+    
+    selected_country = request.GET.get('country', '')
+
+    print(selected_country)
+    if selected_country is None:
+        selected_country = ''
+        
+    if selected_country:
+        preview_place_list_base_url = preview_place_list_base_url.replace('**"', f'**"}},{{"name":"placeCountrySearchTerm","value":"{selected_country}"')
+
+    print(preview_place_list_base_url)
 
     # För devmiljön
     # place_url = 'http://130.238.171.238:38081/alvin/rest/record/alvin-place'
     # record_url = 'http://130.238.171.238:38081/alvin/rest/record/test'
 
-    response = requests.get(preview_place_list_url, headers=xml_headers_list)
-
+    
+    response = requests.get(preview_place_list_base_url, headers=xml_headers_list)
+    context = {}
+    
     if response.status_code == 200:
         alvin_place_search_xml = etree.XML(response.content)
 
         paginated_metadata = [
                 {"authority_names":list(zip(place.xpath("./authority/geographic/text()"), place.xpath("./authority/@lang"))),
                 "variant_names":list(zip(place.xpath("./variant/geographic/text()"), place.xpath("./variant/@lang"))),
-                "id":place.find("./recordInfo/id").text
+                "id":place.find("./recordInfo/id").text,
                 } for place in alvin_place_search_xml.iter("place")
             ]
         
+        countries = list(set(country.text for country in alvin_place_search_xml.xpath("//country")))
+
         paginator = Paginator(paginated_metadata, 1)
         
         page_number = request.GET.get('page')
@@ -43,6 +58,7 @@ def alvin_place_list(request):
             "metadata":metadata,
             "xml":response.content,
             "page_obj":page_obj,
+            "countries":countries
         }
 
     return render(request, "alvin_place_list/alvin_place_list.html", context)

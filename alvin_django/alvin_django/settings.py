e3e4d1784b5a5f72daf7837b87842ab98487f4c3
Marcus Viden <marcus.viden@ub.uu.se>
2025-06-03 15:55:46 +0200

    some test changes for prod

--- alvin_django/alvin_django/settings.py
+++ alvin_django/alvin_django/settings.py
@@ -10,14 +10,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-0$)1vy3^hku+w05m==8!t2+&l8kp5%rnk=&9fsql%5$f9jb6%o'
-SECRET_KEY =  ${{ secrets.DJANGO_SECRET_KEY }}
+SECRET_KEY =  os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "http://127.0.0.1:8000,http://localhost:8000").split(",")
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "http://127.0.0.1:8000,http://localhost:8000,http://localhost:80").split(",")
 
 # Application definition
+
+ASGI_APPLICATION = "alvin_django.asgi.application"
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -25,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'corsheaders',
     'django.contrib.staticfiles',
     'alvin_info',
     'alvin_viewer',
@@ -32,7 +37,6 @@ INSTALLED_APPS = [
     'search',
     'tailwind',
     'theme',
-    'django_browser_reload',
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -47,12 +51,12 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware', # Localization
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'alvin_django.urls'
@@ -60,7 +64,7 @@ ROOT_URLCONF = 'alvin_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['../alvin_django/templates'],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,16 +77,22 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'alvin_django.wsgi.application'
+#WSGI_APPLICATION = 'alvin_django.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': ':memory:',
     }
 }
 
@@ -121,11 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "theme/static/",
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = "/vol/static"
+#STATICFILES_DIRS = os.path.join(BASE_DIR, "theme/static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

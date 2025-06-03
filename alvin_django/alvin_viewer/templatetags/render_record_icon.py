3510bbd96d2416e51f5026315969e635be9ef62a
TheExtrapolatingSnowYakShow <kentingemarbergman@gmail.com>
2025-06-03 15:46:52 +0200

    Hantering av static-filer
    Hårdkodade länkar till statiska filer hanteras nu genom DTL.

--- alvin_django/alvin_viewer/templatetags/render_record_icon.py
+++ alvin_django/alvin_viewer/templatetags/render_record_icon.py
@@ -6,39 +6,39 @@ register = template.Library()
 def render_record_icon(metadata):
     if metadata["record_type"] == 'alvin-place':
         return {
-            'icon_path': '/static/img/authorityTypes/place.svg',
+            'icon_path': '/img/authorityTypes/place.svg',
             'icon_alt': 'Plats',
             'label': 'Plats',
         }
     elif metadata["record_type"] == 'alvin-person':
         return {
-            'icon_path': '/static/img/authorityTypes/person.svg',
+            'icon_path': '/img/authorityTypes/person.svg',
             'icon_alt': 'Person',
             'label': 'Person'
         }
     elif metadata["record_type"] == 'alvin-organisation':
         return {
-            'icon_path': '/static/img/authorityTypes/organisation.svg',
+            'icon_path': '/img/authorityTypes/organisation.svg',
             'icon_alt': 'Organisation',
             'label': 'Organisation',
         }
     elif metadata["record_type"] == 'alvin-work':
         return {
-            'icon_path': '/static/img/authorityTypes/work.svg',
+            'icon_path': '/img/authorityTypes/work.svg',
             'icon_alt': 'Verk',
-            'extra_icon_path': f'/static/img/recordTypes/{metadata["form_of_work"]}.svg',
+            'extra_icon_path': f'/img/recordTypes/{metadata["form_of_work"]}.svg',
             'extra_icon_alt': f'{metadata["form_of_work"]}',
             'label': f'Verk, {metadata["form_of_work"]}',
         }
     elif metadata["type_of_resource"] == 'img':
         return {
-            'icon_path': f'/static/img/recordTypes/image.svg',
+            'icon_path': f'/img/recordTypes/image.svg',
             'icon_alt': 'Bild',
             'label': 'Bild',
         }
     elif metadata["type_of_resource"] == 'txt':
         return {
-            'icon_path': f'/static/img/recordTypes/text.svg',
+            'icon_path': f'/img/recordTypes/text.svg',
             'icon_alt': 'Text',
             'label': 'Text',
         }

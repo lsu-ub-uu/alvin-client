9bdaffd1999b7cbbf1a736ecc00147228e6e25a4
olovm <olov.mckie@ub.uu.se>
2025-06-16 13:58:36 +0000

    changing /static/ to static/

--- alvin_django/alvin_django/settings.py
+++ alvin_django/alvin_django/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = Path(os.environ.get('STATIC_ROOT', '/vol/static'))
 STATICFILES_DIRS = [
     BASE_DIR / "theme/static/",

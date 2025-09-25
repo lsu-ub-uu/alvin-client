8e11ed38f65d9ade590464c33a08bb46f3cb5591
TheExtrapolatingSnowYakShow <kentingemarbergman@gmail.com>
2025-09-25 14:40:58 +0200

    Deleted old theme folder
    As the update to verison 4 of Tailwind now works fine I can delete the backup-folder.

--- alvin_django/theme-backup/static/js/map.js
+++
@@ -1,7 +0,0 @@
-const data = document.currentScript.dataset;
-var map = L.map('map').setView([data.latitude, data.longitude], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-L.marker([data.latitude,data.longitude]).addTo(map);

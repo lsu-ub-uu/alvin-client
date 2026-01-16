09b9d5e994828a247b320fdf693ae8c92b5ac2c4
TheExtrapolatingSnowYakShow <kentingemarbergman@gmail.com>
2026-01-16 12:08:12 +0100

    Changed static URL

--- alvin_django/alvin_django/settings.py
+++ alvin_django/alvin_django/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = Path(os.environ.get('STATIC_ROOT', '/vol/static'))
 STATICFILES_DIRS = [
     BASE_DIR / "theme/static/",

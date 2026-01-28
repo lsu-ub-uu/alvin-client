cf67ffe37c92d908b6c26e90a4d6ddffc099eb19
TheExtrapolatingSnowYakShow <kentingemarbergman@gmail.com>
2026-01-28 14:54:39 +0100

    Tidying and integrating side and top nav

--- alvin_django/alvin_info/urls_old.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path("", views.start, name="start"),
-  path("about/", views.about, name="about"),
-  path("copyright/", views.copyright, name="copyright"),
-  path("kontakt/", views.kontakt, name="contact"),
-]

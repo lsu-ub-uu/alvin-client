f9ddcb80ed08556474fc7f7bcb8b034b831b4b9c
TheExtrapolatingSnowYakShow <kentingemarbergman@gmail.com>
2026-09-11 14:14:48 +0200

    Update iiif.py

--- alvin_django/alvin_viewer/views/iiif.py
+++ alvin_django/alvin_viewer/views/iiif.py
@@ -12,130 +12,137 @@ def _to_int(value, default):
         return int(default)
 
 def iiif_manifest(request, record_id: str):
-    api = AlvinAPI()
     try:
-        record_xml = api.get_record_xml("alvin-record", record_id)
-    except Exception as e:
-        raise Http404(str(e))
-    if record_xml is None:
-        raise Http404(_("Record not found."))
-
-    manifest_base = request.build_absolute_uri(request.path)
-    if not manifest_base.endswith("/"):
-        manifest_base += "/"
-
-    # Extract record metadata
-    record = extract(record_xml)
-    main_label = record.main_title.display if record.main_title else None
-
-    canvases = []
-    idx = 0
-
-    for f in record_xml.xpath("data/record/fileSection/fileGroup/file"):
+        api = AlvinAPI()
         try:
-            file_xml = api.fetch_file_xml(
-                f.findtext("fileLocation/actionLinks/read/url")
-            )
-        except Exception:
-            continue
-        if file_xml is None:
-            continue
-        
-        binary = file_xml.find("data/binary")
-        raw_binary_type = binary.get("type") if binary is not None else None
-        binary_type = raw_binary_type.strip().capitalize() if raw_binary_type else None
+            record_xml = api.get_record_xml("alvin-record", record_id)
+        except Exception as e:
+            raise Http404(str(e))
+        if record_xml is None:
+            raise Http404(_("Record not found."))
 
-        mime_type = file_xml.findtext("data/binary/master/master/mimeType")
+        manifest_base = request.build_absolute_uri(request.path)
+        if not manifest_base.endswith("/"):
+            manifest_base += "/"
 
-        iiif_server = f"{settings.EXTERNAL_ACCESS_URL}/iiif/"
-        ident = file_xml.findtext("otherProtocols/iiif/identifier")
-        
-        # Only build IIIF canvas entries for image binaries
-        if binary_type != "Image":
-            continue
+        # Extract record metadata
+        record = extract(record_xml)
+        main_label = record.main_title.display if record.main_title else None
 
-        if not iiif_server or not ident:
-            continue
-        
-        iiif_server = iiif_server.strip()
-        ident = ident.strip()
+        canvases = []
+        idx = 0
 
-        if not iiif_server.startswith("http"):
-            iiif_server = f"https://{iiif_server}"
+        for f in record_xml.xpath("data/record/fileSection/fileGroup/file"):
+            try:
+                file_xml = api.fetch_file_xml(
+                    f.findtext("fileLocation/actionLinks/read/url")
+                )
+            except Exception as e:
+                raise Http404(_(f"Could not fetch xml: {e}"))
+                # continue
+            if file_xml is None:
+                raise Http404(_(f"The file xml is empty: {e}"))
+                # continue
+            
+            binary = file_xml.find("data/binary")
+            raw_binary_type = binary.get("type") if binary is not None else None
+            binary_type = raw_binary_type.strip().capitalize() if raw_binary_type else None
 
-        image_service_id = f"{iiif_server.rstrip('/')}/{ident.strip('/')}"
-        raster_url = f"{image_service_id}/full/max/0/default.jpg"
+            mime_type = file_xml.findtext("data/binary/master/master/mimeType")
 
-        idx += 1
+            iiif_server = f"{settings.EXTERNAL_ACCESS_URL}/iiif/"
+            ident = file_xml.findtext("otherProtocols/iiif/identifier")
+            
+            # Only build IIIF canvas entries for image binaries
+            if binary_type != "Image":
+                raise Http404(_(f"Not an image: {binary_type}"))
+                #continue
 
-        canvas_id = urljoin(manifest_base, f"canvas/{idx}")
-        anno_page_id = urljoin(canvas_id + "/", "page")
-        anno_id = urljoin(anno_page_id + "/", "anno")
-        
-        original_url = f.findtext("fileLocation/linkedRecord/binary/jp2/jp2/actionLinks/read/url")
-                
-        body = {
-            "id": raster_url,
-            "type": binary_type,
-            "format": mime_type,
-            "service": [{
-                "id": image_service_id,
-                "type": "ImageService2",
-                "profile": "http://iiif.io/api/image/2/level2.json"
-            }]
+            if not iiif_server or not ident:
+                raise Http404(_(f"No server or no id: {iiif_server}, {ident}"))
+                #continue
+            
+            iiif_server = iiif_server.strip()
+            ident = ident.strip()
+
+            if not iiif_server.startswith("http"):
+                iiif_server = f"https://{iiif_server}"
+
+            image_service_id = f"{iiif_server.rstrip('/')}/{ident.strip('/')}"
+            raster_url = f"{image_service_id}/full/max/0/default.jpg"
+
+            idx += 1
+
+            canvas_id = urljoin(manifest_base, f"canvas/{idx}")
+            anno_page_id = urljoin(canvas_id + "/", "page")
+            anno_id = urljoin(anno_page_id + "/", "anno")
+            
+            original_url = f.findtext("fileLocation/linkedRecord/binary/jp2/jp2/actionLinks/read/url")
+                    
+            body = {
+                "id": raster_url,
+                "type": binary_type,
+                "format": mime_type,
+                "service": [{
+                    "id": image_service_id,
+                    "type": "ImageService2",
+                    "profile": "http://iiif.io/api/image/2/level2.json"
+                }]
+            }
+
+            measures = {
+                    "height": _to_int(file_xml.findtext(".//height"), 1000),
+                    "width": _to_int(file_xml.findtext(".//width"), 1000)
+                    }
+            
+            if binary_type == "Image":
+                body.update(measures) 
+            
+            canvas = {
+                "id": canvas_id,
+                "type": "Canvas",
+                "items": [
+                    {
+                        "id": anno_page_id,
+                        "type": "AnnotationPage",
+                        "items": [
+                            {
+                                "id": anno_id,
+                                "type": "Annotation",
+                                "motivation": "painting",
+                                "target": canvas_id,
+                                "body": body,
+                            }
+                        ],
+                    }
+                ],
+                "rendering": [
+                    {
+                        "id": original_url,
+                        "type": binary_type,
+                        "format": mime_type,
+                        "label": {"none": ["Download original"]},
+                    }
+                ] if original_url else [],
+            }
+
+            if binary_type == "Image":
+                canvas.update(measures)
+
+            canvases.append(canvas)
+
+        if not canvases:
+            raise Http404(_("No IIIF-capable files found on this record."))
+
+        manifest = {
+            "@context": "http://iiif.io/api/presentation/3/context.json",
+            "id": request.build_absolute_uri(),
+            "type": "Manifest",
+            "label": {"none": [main_label]} if main_label else {"none": [str(record_id)]},
+            "items": canvases,
         }
-
-        measures = {
-                "height": _to_int(file_xml.findtext(".//height"), 1000),
-                "width": _to_int(file_xml.findtext(".//width"), 1000)
-                }
-        
-        if binary_type == "Image":
-            body.update(measures) 
-        
-        canvas = {
-            "id": canvas_id,
-            "type": "Canvas",
-            "items": [
-                {
-                    "id": anno_page_id,
-                    "type": "AnnotationPage",
-                    "items": [
-                        {
-                            "id": anno_id,
-                            "type": "Annotation",
-                            "motivation": "painting",
-                            "target": canvas_id,
-                            "body": body,
-                        }
-                    ],
-                }
-            ],
-            "rendering": [
-                {
-                    "id": original_url,
-                    "type": binary_type,
-                    "format": mime_type,
-                    "label": {"none": ["Download original"]},
-                }
-            ] if original_url else [],
-        }
-
-        if binary_type == "Image":
-            canvas.update(measures)
-
-        canvases.append(canvas)
-
-    if not canvases:
-        raise Http404(_("No IIIF-capable files found on this record."))
-
-    manifest = {
-        "@context": "http://iiif.io/api/presentation/3/context.json",
-        "id": request.build_absolute_uri(),
-        "type": "Manifest",
-        "label": {"none": [main_label]} if main_label else {"none": [str(record_id)]},
-        "items": canvases,
-    }
+    except Exception as e:
+        raise Http404(_(f"Something went wrong: {e}"))
 
     return JsonResponse(
         manifest,
